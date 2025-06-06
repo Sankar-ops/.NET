21e48a4ed72a51671c69a3d5c26983651551a265
Sankar-ops <80885217+Sankar-ops@users.noreply.github.com>
2025-06-06 04:44:34 +0000

    Update Startup.cs

--- dotnet-hello-world-master/hello-world-api/Startup.cs
+++ dotnet-hello-world-master/hello-world-api/Startup.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace hello_world_api
 {
@@ -20,21 +14,26 @@ namespace hello_world_api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // Register services
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers(); // Use AddControllers() instead of AddMvc()
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // Configure middleware
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) // <-- FIX: IWebHostEnvironment
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName == "Development")
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // Enables attribute-based routing
+            });
         }
     }
 }
